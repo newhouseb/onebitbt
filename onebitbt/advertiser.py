33d8e795fceafa659a5514ae4a813aedd34acb90
Ben Newhouse <newhouseb@gmail.com>
2021-04-11 19:29:14 -0400

    Simplify and just run things at 5gsps rather than 6 (thereby using a more stable clock)

--- onebitbt/advertiser.py
+++ onebitbt/advertiser.py
@@ -27,67 +27,31 @@ from serialcommander.toggler import Toggler
 # instead precompute it
 PACKET =[22, 21, 20, 21, 20, 21, 20, 21, 20, 23, 10, 12, 13, 12, 15, 2, 4, 7, 24, 16, 8, 2, 4, 7, 26, 27, 6, 5, 3, 9, 22, 21, 20, 23, 8, 2, 3, 9, 22, 23, 8, 0, 24, 18, 19, 25, 6, 7, 26, 28, 29, 27, 1, 14, 15, 2, 4, 7, 24, 18, 19, 30, 31, 16, 8, 2, 3, 14, 15, 2, 3, 9, 17, 30, 31, 18, 19, 25, 6, 5, 3, 14, 15, 2, 3, 14, 13, 12, 13, 11, 17, 30, 31, 18, 20, 21, 19, 25, 1, 9, 22, 21, 19, 30, 31, 16, 10, 12, 15, 0, 26, 27, 1, 9, 17, 30, 31, 18, 19, 25, 1, 14, 13, 12, 13, 11, 17, 30, 29, 28, 29, 27, 1, 14, 13, 11, 22, 23, 8, 0, 26, 27, 6, 5, 4, 7, 24, 16, 8, 2, 3, 9, 17, 25, 6, 7, 26, 28, 31, 16, 8, 0, 24, 18, 20, 21, 20, 23, 10, 11, 22, 21, 19, 25, 1, 14, 15, 0, 24, 16, 8, 2, 4, 5, 4, 7, 24, 18, 20, 23, 8, 2, 3, 9, 17, 30, 31, 16, 8, 0, 24, 18, 19, 30, 29, 28, 31, 18, 20, 21, 20, 23, 10, 11, 17, 30, 29, 28, 31, 16, 8, 0, 26, 27, 1, 14, 15, 0, 26, 27, 6, 7, 24, 18, 19, 25, 6, 7, 24, 16, 8, 0, 26, 28, 29, 27, 1, 9, 17, 25, 1, 9, 17, 30, 29, 28, 29, 27, 6, 5, 3, 14, 15, 0]
 
-TABLE = np.load('data/gmsk_2402e6_6e9.npy').flatten()
+# If you want to test a sine wave, this will give you that
+#PACKET = [0, 24, 16, 8]*100
+
+TABLE = np.load('data/gmsk_2402e6_5e9.npy').flatten()
 
 class BLEAdvertiser(Elaboratable):
     def __init__(self):
         self.refclk = Signal()
-        self.serdes = get_serdes_implementation()(line_rate=6.0e9, refclk_freq=120e6, internal_refclk=self.refclk)
-        self.uart = UART(int(25e6/115200))
+        self.serdes = get_serdes_implementation()(line_rate=5.0e9, refclk_freq=125e6) #, internal_refclk=self.refclk)
 
     def elaborate(self, platform):
         m = Module()
-
-        output_enable = Toggler()
-        m.submodules.uart = self.uart
-        m.submodules.commander = Commander(self.uart, {
-            '1': output_enable,
-        })
-        # Connect the UART ports to the real pins
-        m.d.comb += [
-            platform.request('uart_tx').eq(self.uart.tx_o),
-            self.uart.rx_i.eq(platform.request('uart_rx'))
-        ]
-
         m.submodules.serdes = serdes = self.serdes
-
-        # The SERDES PLL is fairly coarse, as in it can only multiply
-        # the reference clock frequency by a set number of integers (which
-        # max out at 20 and 25). Given the board I'm using has a 125MHz
-        # reference clock, that only allows 5GHz or 6.25GHz line rates.
-        # We want higher than 5GHz because the nyquist sampling and we want
-        # something that results in a samples per symbol rate that is evenly
-        # divisible by 20. For a 1msps signal, the samples per symbol at 6.25GHz
-        # is 6250, which is not divisible by 20. So instead, I synthesize a ref
-        # clock at 120MHz which is then synthesized to a 6GHz line rate, resulting
-        # in 6000 samples per symbol, which is evently divisible by 20.
-        clk125 = Signal()
-        m.submodules.clock = Instance('IBUFDS_GTE2',
-            o_O=clk125,
-            i_I=platform.request('clk_p', dir='-'),
-            i_IB=platform.request('clk_n', dir='-'),
-            i_CEB=0)
-        m.submodules.ref120 = ref120 = Reference120MhzClock()
-        m.d.comb += [
-            ref120.clk125.eq(clk125),
-            self.refclk.eq(ref120.clk120)
-        ]
 
         # The symbol table reads the corresponding symbols for each
         # symbol in PACKET and outputs them on tx_data
         m.submodules.symboltable = symboltable = SymbolTable(
             table=pack_mem(TABLE, width=20), 
-            packet=Memory(width=8, depth=len(PACKET), init=PACKET), 
-            samples_per_symbol=int(6e9/1e6), 
+            packet=Memory(width=16, depth=len(PACKET), init=[int(i) for i in np.array(PACKET) * 5000/20]), 
+            samples_per_symbol=int(5e9/1e6), 
             tx_domain="tx")
-        m.d.comb += symboltable.packet_length.eq(len(PACKET))
-
-        # Maker the output toggleable over serial
-        with m.If(~output_enable.output):
-            m.d.comb += serdes.tx_data.eq(symboltable.tx_data)
-        with m.Else():
-            m.d.comb += serdes.tx_data.eq(0)
-        m.d.comb += platform.request("led").eq(~output_enable.output)
+        m.d.comb += [
+            symboltable.packet_length.eq(len(PACKET)),
+            serdes.tx_data.eq(symboltable.tx_data)
+        ]
 
         # Quick state machine to transmit and then wait a bit before
         # transmitting again
